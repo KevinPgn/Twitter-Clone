a35f745a126d465d99f8def5134644446b3736ca
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-03 13:04:34 +0200

    sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,11 +1,15 @@
 import { ButtonPost } from "./ButtonPost"
 import { Links } from "./Links"
 import { Authentication } from "./Authentication"
+import Link from "next/link"
 
 export const Sidebar = () => {
   return <div className="w-[275px] justify-between py-3 flex flex-col">
     <div className="flex flex-col">
-        <h2 className="text-xl font-bold cursor-pointer">TwitterClone</h2>    
+        <Link href="/">
+            <img src="/logo-twitter-x.jpg" alt="logo" className="w-9 h-9 cursor-pointer rounded-full" />
+        </Link>
+        
         <Links />
         <ButtonPost />
     </div>
