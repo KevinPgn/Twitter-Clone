a0bcc03d0c1e04baa86dd450e5b0d393f9cc7aa2
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-03 13:20:53 +0200

    design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Categories } from "@/components/homePage/Categories";
 import { SidebarRight } from "@/components/sidebarRight/SidebarRight";
 
 export default function Home() {
   return (
     <div className="flex flex-1">
       <main className="flex-1 h-full border-l border-r border-white/10">
+        <Categories />
       </main>
       <SidebarRight />
     </div>
