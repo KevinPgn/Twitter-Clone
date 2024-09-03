a6045e2d861eeb175878044a6c0d6ac1f9f882b7
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-03 13:49:28 +0200

    Categories

--- src/components/homePage/Categories.tsx
+++ src/components/homePage/Categories.tsx
@@ -5,7 +5,7 @@ export const Categories = () => {
     <div className="flex items-center justify-between text-center h-full">
       <span className="text-md relative
       after:content-[''] after:absolute after:bottom-0 after:left-[25%] after:w-[50%] after:h-[2px] after:bg-blue-500 after:rounded-full
-      font-bold hover:after:w-full duration-300 w-[50%] py-3 cursor-pointer">Pour vous</span>
+      font-bold hover:bg-white/10 duration-75 w-[50%] py-3 cursor-pointer">Pour vous</span>
       <span className="text-md text-gray-400 hover:bg-white/10 w-[50%] duration-75 py-3 cursor-pointer">Abonnements</span>
     </div>
   </div>
