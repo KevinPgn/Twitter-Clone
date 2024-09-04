05a249d79f64aee1a472e3eb5d0d2f82a14d9201
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-04 13:45:57 +0200

    tweets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,49 @@
+import { AllTweets } from "@/components/homePage/AllTweets";
 import { Categories } from "@/components/homePage/Categories";
 import { FormCreateTweet } from "@/components/homePage/FormCreateTweet";
 import { SidebarRight } from "@/components/sidebarRight/SidebarRight";
 import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
 
 export default async function Home() {
   const session = await auth()
   const user = session?.user
+  
+  const tweets = await prisma.tweet.findMany({
+    select: {
+      id: true,
+      author: true,
+      content: true,
+      imageUrl: true,
+      createdAt: true,
+      views: true,
+      _count: {
+        select: {
+          comments: true,
+          likes: true,
+          bookmarks: true,
+          retweets: true,
+        }
+      }
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    take: 10
+  })
 
   return (
     <div className="flex flex-1">
       <main className="flex-1 h-full border-l border-r border-white/10">
         <Categories />
         <FormCreateTweet user={user}/>
+        
+        {tweets.length > 0 ? (
+          tweets.map((tweet) => (
+            <AllTweets key={tweet.id} tweet={tweet}/>
+          ))
+        ): null}
+
       </main>
       <SidebarRight />
     </div>
---
+++ src/components/homePage/AllTweets.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link"
+import { formatDistanceToNow } from "date-fns"
+import { FiMessageCircle, FiHeart, FiBookmark, FiShare } from "react-icons/fi";
+import { AiOutlineRetweet } from "react-icons/ai";
+import { IoIosStats } from "react-icons/io";
+
+export const AllTweets = ({tweet}: {tweet: any}) => {
+  return <div className="flex items-start gap-3 px-4 border-b border-white/10 p-3">
+    <Link href={`/profile/${tweet.author.id}`}>
+      <img src={tweet.author.image} alt="author post image" className="w-10 h-10 cursor-pointer rounded-full" />
+    </Link>
+    <div className="flex flex-col flex-1">
+      <div className="flex items-center gap-1">
+        <Link href={`/profile/${tweet.author.id}`} className="flex items-center gap-3">
+          <span className="text-sm text-white font-semibold">{tweet.author.name}</span>
+          <span className="text-sm text-gray-500 font-normal">
+            {formatDistanceToNow(tweet.createdAt, {addSuffix: false}).replace('about ', '').replace(' hour', 'h')}
+          </span>
+        </Link>
+      </div>
+      <p className="text-sm text-white/90 mt-3">{tweet.content}</p>
+      {tweet.imageUrl && <img src={tweet.imageUrl} alt="tweet image" className="w-full h-full object-cover mt-3 rounded-xl" />}
+      
+      <div className="flex items-center justify-between mt-3">
+        <div className="flex items-center group cursor-pointer gap-2 text-gray-400">
+          <FiMessageCircle size={19} className="group-hover:text-blue-500 duration-75"/>
+          <span className="text-sm group-hover:text-blue-500 duration-75 text-gray-400 font-normal">{tweet._count.comments}</span>
+        </div>
+        <div className="flex items-center group cursor-pointer gap-2 text-gray-400">
+          <AiOutlineRetweet size={19} className="group-hover:text-green-500 duration-75"/>
+          <span className="text-sm group-hover:text-green-500 duration-75 text-white/80 font-normal">{tweet._count.retweets}</span>
+        </div>
+        <div className="flex items-center group cursor-pointer gap-2 text-gray-400">
+          <FiHeart size={19} className="group-hover:text-red-500 duration-75"/>
+          <span className="text-sm group-hover:text-red-500 duration-75 font-normal">{tweet._count.likes}</span>
+        </div>
+        <div className="flex items-center group cursor-pointer gap-2 text-gray-400">
+          <IoIosStats size={19} className="group-hover:text-blue-500 duration-75"/>
+          <span className="text-sm group-hover:text-blue-500 duration-75 font-normal">{tweet.views}</span>
+        </div>
+
+        <div className="flex items-center gap-5 text-gray-400">
+          <FiBookmark size={19} className="cursor-pointer hover:text-blue-500 duration-75"/>
+          <FiShare size={19} className="cursor-pointer hover:text-blue-500 duration-75"/>
+        </div>
+      </div>
+    </div>
+  </div>
+}
