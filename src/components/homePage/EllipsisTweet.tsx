7b3a3c73fbf9acb897ef6b11397652b323c1df9f
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-04 18:52:18 +0200

    ellipsis

--- src/components/homePage/EllipsisTweet.tsx
+++ src/components/homePage/EllipsisTweet.tsx
@@ -2,6 +2,7 @@
 import { Ellipsis } from "lucide-react"
 import { useState } from "react"
 import { Button } from "../ui/button"
+import { Pen, Trash2 } from "lucide-react"
 
 export const EllipsisTweet = ({tweetId}: {tweetId: string}) => {
   const [isOpen, setIsOpen] = useState(false)
@@ -9,8 +10,14 @@ export const EllipsisTweet = ({tweetId}: {tweetId: string}) => {
   return <div className="absolute top-0 right-0">
       <Ellipsis size={19} className="text-gray-500 cursor-pointer" onClick={() => setIsOpen(!isOpen)} />
       {isOpen && <div className="absolute w-[150px] bg-black border border-white/10 rounded-xl shadow-md shadow-white/10 p-2 top-5 right-5">
-        <Button variant="ghost" className="w-full text-white/80">Edit</Button>
-        <Button variant="ghost" className="w-full text-white/80">Delete</Button>
+        <Button variant="ghost" className="w-full hover:bg-blue-500/20 hover:text-blue-500 text-white flex justify-start">
+          <Pen size={19} className="mr-2" />
+          Edit
+        </Button>
+        <Button variant="ghost" className="w-full hover:bg-red-500/20 hover:text-red-500 text-white flex justify-start">
+          <Trash2 size={19} className="mr-2" />
+          Delete
+        </Button>
       </div>}
     </div>
 }
