59dea62ac678dd649c41150739ac40316db50b29
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-04 14:18:50 +0200

    tweet

--- src/components/homePage/AllTweets.tsx
+++ src/components/homePage/AllTweets.tsx
@@ -6,21 +6,22 @@ import { IoIosStats } from "react-icons/io";
 import { EllipsisTweet } from "./EllipsisTweet";
 
 export const AllTweets = ({tweet, user}: {tweet: any, user: any}) => {
-  return <div className="flex items-start gap-3 px-4 border-b border-white/10 p-3">
+  return <div 
+  className="flex hover:bg-white/5 duration-75 cursor-pointer items-start gap-3 px-4 border-b border-white/10 p-3">
     <Link href={`/profile/${tweet.author.id}`}>
       <img src={tweet.author.image} alt="author post image" className="w-10 h-10 cursor-pointer rounded-full" />
     </Link>
     <div className="flex flex-col flex-1">
       <div className="flex relative items-center gap-1">
         <Link href={`/profile/${tweet.author.id}`} className="flex items-center gap-3">
-          <span className="text-sm text-white font-semibold">{tweet.author.name}</span>
+          <span className="text-sm text-white hover:underline font-semibold">{tweet.author.name}</span>
           <span className="text-sm text-gray-500 font-normal">
             {formatDistanceToNow(tweet.createdAt, {addSuffix: false}).replace('about ', '').replace(' hour', 'h').replace(' minutes', 'm').replace(' minute', 'm').replace(' seconds', 's').replace(' second', 's')}
           </span>
         </Link>
         {user && user.id === tweet.author.id && <EllipsisTweet tweetId={tweet.id} />}
       </div>
-      <p className="text-md text-white/90 mt-3">{tweet.content}</p>
+      <p className="text-md text-white/90 mt-1">{tweet.content}</p>
       {tweet.imageUrl && <img src={tweet.imageUrl} alt="tweet image" className="w-full h-full object-cover mt-3 rounded-xl" />}
       
       <div className="flex items-center justify-between mt-3">
--- src/components/homePage/EllipsisTweet.tsx
+++ src/components/homePage/EllipsisTweet.tsx
@@ -1,8 +1,16 @@
 "use client"
 import { Ellipsis } from "lucide-react"
+import { useState } from "react"
+import { Button } from "../ui/button"
 
 export const EllipsisTweet = ({tweetId}: {tweetId: string}) => {
+  const [isOpen, setIsOpen] = useState(false)
+  
   return <div className="absolute top-0 right-0">
-    <Ellipsis size={19} className="text-gray-500 cursor-pointer" />
-  </div>
+      <Ellipsis size={19} className="text-gray-500 cursor-pointer" onClick={() => setIsOpen(!isOpen)} />
+      {isOpen && <div className="absolute w-[150px] bg-black border border-white/10 rounded-xl shadow-md shadow-white/10 p-2 top-5 right-5">
+        <Button variant="ghost" className="w-full text-white/80">Edit</Button>
+        <Button variant="ghost" className="w-full text-white/80">Delete</Button>
+      </div>}
+    </div>
 }
