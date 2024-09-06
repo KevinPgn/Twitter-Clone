c2eda13729f5b9e50adee184e0b07f790f588de7
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-06 13:47:02 +0200

    follow

--- src/components/profile/BtnFollow.tsx
+++ src/components/profile/BtnFollow.tsx
@@ -1,15 +1,21 @@
 "use client"
 import { Button } from "../ui/button"
-import { followUser } from "@/server/Actions"
+import { followUser, unfollowUser } from "@/server/Actions"
 
 export const BtnFollow = ({userId, isFollowing}: {userId: string, isFollowing: boolean}) => {
   return <>
-    <Button
+   {isFollowing ? <Button
+    onClick={async () => {
+      await unfollowUser({userId})
+    }}
+    variant="destructive" className="text-sm mt-2 border border-white/10 px-4 py-1 rounded-full">
+      Unfollow
+    </Button> : <Button
     onClick={async () => {
       await followUser({userId})
     }}
     variant="outline" className="text-sm text-black mt-2 border border-white/10 px-4 py-1 rounded-full">
-      {isFollowing ? "Unfollow" : "Follow"}
-    </Button>
+      Follow
+    </Button>}
   </>
 }
